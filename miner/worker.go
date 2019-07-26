5e1bfaf21b14d1e698c4b332ad501bca7fd0086b
liuxing <liuxing@juzix.net>
2019-07-26 13:48:15 +0800

    add metrics meter miner/block/mined

--- miner/worker.go
+++ miner/worker.go
@@ -816,7 +816,7 @@ func (w *worker) resultLoop() {
 			// Broadcast the block and announce chain insertion event
 			w.mux.Post(core.NewMinedBlockEvent{Block: block})
 			NewMinedBlockMeter.Mark(1)
-			MinedBlockTxCountHistogram.Update(block.Transactions().Len())
+			MinedBlockTxCountHistogram.Update(len(block.Transactions()))
 
 			var events []interface{}
 			switch stat {
